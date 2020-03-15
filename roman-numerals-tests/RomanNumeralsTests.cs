b593e9d567c524719626ee9174fe8bc7fb8a7ca3
Mattsi <mattsi.jansky@codurance.com>
2020-03-15 16:26:20 +0000

    Initial commit. One failing test.

---
+++ roman-numerals-tests/RomanNumeralsTests.cs
@@ -0,0 +1,21 @@
+using System;
+using NUnit.Framework;
+using roman_numerals;
+
+namespace roman_numerals_tests
+{
+    public class RomanNumeralsTests
+    {
+        private static readonly (int, string)[] DecimalsMappedToRomanNumerals =
+        {
+            (1, "I")
+        };
+        
+        [Theory, TestCaseSource(nameof(DecimalsMappedToRomanNumerals))]
+        public void ShouldConvertDecimalsToRomanNumerals((int decimalFormat, string romanFormat) testCase)
+        {
+            var result = RomanNumerals.ParseFromDecimal(testCase.decimalFormat);
+            Assert.AreEqual(testCase.romanFormat, result);
+        }
+    }
+}
---
+++ roman-numerals/RomanNumerals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace roman_numerals
+{
+    public static class RomanNumerals
+    {
+        public static string ParseFromDecimal(int n)
+        {
+            return null;
+        } 
+    }
+}
