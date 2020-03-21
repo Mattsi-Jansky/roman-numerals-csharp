8a07875dc6964f2c08c2025bb814c5d636ab71cb
Mattsi <mattsi.jansky@codurance.com>
2020-03-21 17:13:40 +0000

    Transform scalar -> array implement x=10

--- roman-numerals/RomanNumerals.cs
+++ roman-numerals/RomanNumerals.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace roman_numerals
 {
     public static class RomanNumerals
     {
+        private static readonly IDictionary<int, char> Numerals = new Dictionary<int, char>()
+        {
+            { 5, 'V'},
+            { 10, 'X'}
+        };
+        
         public static string ParseFromDecimal(int n)
         {
-            if (n == 5) return "V";
+            if (Numerals.ContainsKey(n)) return Numerals[n].ToString();
             return new string(Enumerable.Range(1, n).Select(_ => 'I').ToArray());
         } 
     }
